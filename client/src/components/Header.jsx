0f13775f48236abbc9a44635586aa3ed4b871bd3
Chris-yee <chris-yee@Chris-yeedeMacBook-Air.local>
2026-03-28 19:34:14 +0800

    feat: add Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>旅行打卡海报</h1>
+      <p>输入城市，生成小红书风格旅行路线打卡图</p>
+    </header>
+  );
+}
+
+export default Header;
